ffb45e093f1116cf4a299a1c17e884c0bd8a207e
snd-so <sssoneill@gmail.com>
2025-02-11 11:04:48 -0500

    Update sketch.js__centering

--- sketch.js
+++ sketch.js
@@ -18,27 +18,25 @@ var env, env1, env2;
 var chooser = 1;
 var adder;
 
-
 // ------------------------------------------------------------------------------------
 
-function preload() { // for sounds / images that need time before playing 
+function preload() {
+  // for sounds / images that need time before playing
 
-  song = loadSound('sounds/rec.mp3');
-  song1 = loadSound('sounds/BEAPS.mp3');
-  song2 = loadSound('sounds/CAN2.mp3');
-  song3 = loadSound('sounds/noise2.mp3');
-  song4 = loadSound('sounds/cheby.mp3');
-  song5 = loadSound('sounds/bees.mp3');
+  song = loadSound("sounds/rec.mp3");
+  song1 = loadSound("sounds/BEAPS.mp3");
+  song2 = loadSound("sounds/CAN2.mp3");
+  song3 = loadSound("sounds/noise2.mp3");
+  song4 = loadSound("sounds/cheby.mp3");
+  song5 = loadSound("sounds/bees.mp3");
 }
 
 // ------------------------------------------------------------------------------------
 
 function setup() {
-  
-  let scaleFactor = isIOS() ? 2 : 1;  // Increase scale factor for iOS
+  let scaleFactor = isIOS() ? 2 : 1; // Increase scale factor for iOS
   pixelDensity(isIOS() ? 2 : 1); // Boost pixel density on iOS
-  
-  
+
   env = new p5.Env();
   env.setADSR(2.0, 5.0, 0.7, 10); //attacktime,decaytime,suspercent,releaseTime
   env.setRange(0.5, 0); //attaack level, release level
@@ -66,52 +64,54 @@ function setup() {
   song4.amp(env);
   song5.amp(env4);
 
-  let canvasSize = min(windowWidth, windowHeight) * 0.9 * scaleFactor;  // Scale dynamically
+  let canvasSize = min(windowWidth, windowHeight) * 0.9 * scaleFactor; // Scale dynamically
   cnv = createCanvas(canvasSize, canvasSize);
   centerCanvas();
   background(255);
 
-   // Unlock audio context on user interaction
+  // Unlock audio context on user interaction
   userStartAudio();
   getAudioContext().resume(); // Make sure audio context is running
 
-  shapie = new Shapie(TRIANGLE_STRIP, (windowWidth - (width / scaleFactor)) / 2, (windowHeight - (height / scaleFactor)) / 2);
-
+  shapie = new Shapie(
+    TRIANGLE_STRIP,
+    (windowWidth - width / scaleFactor) / 2,
+    (windowHeight - height / scaleFactor) / 2
+  );
 }
 
 // ------------------------------------------------------------------------------------
 
 function draw() {
-
   // duration2 = song2.duration(); // tried this in setup didn't work, why?
-  song.playMode('sustain'); // or sustain / restart
-  song1.playMode('sustain'); // or sustain / restart
-  song2.playMode('sustain'); // or sustain / restart
-  song3.playMode('sustain'); // or sustain / restart
-  song4.playMode('sustain'); // or sustain / restart
-  song5.playMode('sustain'); // or sustain / restart
+  song.playMode("sustain"); // or sustain / restart
+  song1.playMode("sustain"); // or sustain / restart
+  song2.playMode("sustain"); // or sustain / restart
+  song3.playMode("sustain"); // or sustain / restart
+  song4.playMode("sustain"); // or sustain / restart
+  song5.playMode("sustain"); // or sustain / restart
 
   background(255); // spent forever trying to figure out shapes not refreshing, didnt have background
 
   shapie.update();
   shapie.display();
 
-//   for (var i = coords.length - 1; i >= 0; i--) {
-//     coords[i].update();
-//     coords[i].display();
-//     if (coords[i].lifespan2 < 0) { //had changed the name of lifespan, watch that 
-//       coords.splice(i, 1);
+  //   for (var i = coords.length - 1; i >= 0; i--) {
+  //     coords[i].update();
+  //     coords[i].display();
+  //     if (coords[i].lifespan2 < 0) { //had changed the name of lifespan, watch that
+  //       coords.splice(i, 1);
 
-//     }
-//   }
+  //     }
+  //   }
 }
 
 // ------------------------------------------------------------------------------------
 
 function centerCanvas() {
-let scaleFactor = isIOS() ? 2 : 1;  // Match the scaling factor from setup()
-  let x = (windowWidth - (width / scaleFactor)) / 2;
-  let y = (windowHeight - (height / scaleFactor)) / 2;
+  let scaleFactor = isIOS() ? 2 : 1; // Match the scaling factor from setup()
+  let x = (windowWidth - width / scaleFactor) / 2;
+  let y = (windowHeight - height / scaleFactor) / 2;
   cnv.position(x, y);
 }
 
@@ -129,91 +129,112 @@ function isIOS() {
   return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 }
 
-
-var Shapie = function(kind, mx, my) {
-  let scaleFactor = isIOS() ? 2 : 1;  // Match the scaling   factor from setup()
+var Shapie = function (kind, mx, my) {
+  let scaleFactor = isIOS() ? 2 : 1; // Match the scaling   factor from setup()
   this.shapee = kind;
-  this.mx = mx;
-  this.my = my;
+  this.mx = width / 2;
+  this.my = height / 2;
   this.take = false;
 
+  var listpoints = [
+    this.mx + 30,
+    this.mx + 40,
+    this.mx + 50,
+    this.mx + 60,
+    this.mx + 70,
+    this.mx + 22,
+    this.mx + 90,
+    this.mx + 100,
+    this.mx + 66,
+    this.mx + 77,
+  ];
 
-  var listpoints = [30, 40, 50, 60, 70, 22, 90, 100, 66, 77];
-  var listpoints2 = [75, 20, 75, 20, 75, 20, 75, 20, 75, 20];
+  var listpoints2 = [
+    this.my + 75,
+    this.my + 20,
+    this.my + 75,
+    this.my + 20,
+    this.my + 75,
+    this.my + 20,
+    this.my + 75,
+    this.my + 20,
+    this.my + 75,
+    this.my + 20,
+  ];
+  
   var spot = floor(random(10));
   var spot2 = floor(random(10));
   var spot3 = floor(random(10));
 
-
   for (var i = 0; i < 10; i++) {
     var choose = random(15);
     if (choose < 7) {
-      listpoints[i] = floor(random(width * 0.5)); 
+      listpoints[i] = floor(random(width * 0.5)) + width * 0.25;
     }
 
     if (choose > 7) {
-      listpoints2[i] = floor(random(height * 0.5)); 
+      listpoints2[i] = floor(random(height * 0.5)) + height * 0.25;
     }
   }
 
   // ------------------------------------------------------------------------------------
-  
-  this.checkTouch = function(x, y) {
-  if (dist(x, y, listpoints[spot] + this.mx, listpoints2[spot] + this.my) < 20) {
-    this.take = true;
-    this.shuffle();
-    this.playlist();
-    this.coords();
-  }
-};
 
+  this.checkTouch = function (x, y) {
+    if (
+      dist(x, y, listpoints[spot] + this.mx, listpoints2[spot] + this.my) < 20
+    ) {
+      this.take = true;
+      this.shuffle();
+      this.playlist();
+      this.coords();
+    }
+  };
 
-  this.display = function() {
-
+  this.display = function () {
     let scaleFactor = isIOS() ? 2 : 1;
 
- stroke(0, 77, 111);
-  strokeWeight(1.5 * scaleFactor);
-  ellipse(listpoints[spot] * scaleFactor, 
-          listpoints2[spot] * scaleFactor, 
-          14 * scaleFactor);
+    stroke(0, 77, 111);
+    strokeWeight(1.5 * scaleFactor);
+    ellipse(
+      listpoints[spot] * scaleFactor,
+      listpoints2[spot] * scaleFactor,
+      14 * scaleFactor
+    );
 
-  stroke(255, 111, 0);
-  strokeWeight(1.5 * scaleFactor);
-  ellipse(listpoints[spot2]  * scaleFactor, 
-          listpoints2[spot2]* scaleFactor, 
-          14 * scaleFactor);
+    stroke(255, 111, 0);
+    strokeWeight(1.5 * scaleFactor);
+    ellipse(
+      listpoints[spot2] * scaleFactor,
+      listpoints2[spot2] * scaleFactor,
+      14 * scaleFactor
+    );
 
-  stroke(255, 55, 0);
-  strokeWeight(1.5 * scaleFactor);
-  ellipse(listpoints[spot3] * scaleFactor, 
-          listpoints2[spot3] * scaleFactor, 
-          14 * scaleFactor);
+    stroke(255, 55, 0);
+    strokeWeight(1.5 * scaleFactor);
+    ellipse(
+      listpoints[spot3] * scaleFactor,
+      listpoints2[spot3] * scaleFactor,
+      14 * scaleFactor
+    );
 
-
-    
     stroke(111);
-   strokeWeight(1 * scaleFactor);
+    strokeWeight(1 * scaleFactor);
     fill(255);
     beginShape(this.shapee);
-for (let i = 0; i < 10; i++) {
-  vertex((listpoints[i] ) * scaleFactor, (listpoints2[i] ) * scaleFactor);
-}
-endShape();
-  }
+    for (let i = 0; i < 10; i++) {
+      vertex(listpoints[i] * scaleFactor, listpoints2[i] * scaleFactor);
+    }
+    endShape();
+  };
   // ------------------------------------------------------------------------------------
 
-
-  this.update = function() {
-
-
+  this.update = function () {
     if (dist(mouseX, mouseY, listpoints[spot], listpoints2[spot]) < 30) {
       this.take = true;
       this.shuffle();
       this.playlist();
       this.coords();
     }
-
 
     if (dist(mouseX, mouseY, listpoints[spot2], listpoints2[spot2]) < 30) {
       this.take = true;
@@ -229,12 +250,13 @@ endShape();
       this.playlist();
       this.coords();
     }
-  }
+  };
 
   // ------------------------------------------------------------------------------------
 
-  this.coords = function() { // this funciton is inside of the Shapie function but maybe should have been outside
-  
+  this.coords = function () {
+    // this funciton is inside of the Shapie function but maybe should have been outside
+
     // var adder = (random(-40, -10));
     // var chooser = floor(random(1, 4));
     if (chooser == 4) {
@@ -244,56 +266,70 @@ endShape();
     }
 
     if (chooser == 1) {
-      diftc = '';
-      link = 'https://elevatorbath.bandcamp.com';
+      diftc = "";
+      link = "https://elevatorbath.bandcamp.com";
       adder = 0;
     }
     if (chooser == 2) {
-      diftc = '';
-      link = 'https://www.instagram.com/snonll/';
+      diftc = "";
+      link = "https://www.instagram.com/snonll/";
       adder = -10;
     }
     if (chooser == 3) {
-      diftc = '';
-      link = 'https://sssoneill.github.io/wohnklo/';
+      diftc = "";
+      link = "https://sssoneill.github.io/wohnklo/";
       adder = -20;
     }
     if (chooser == 4) {
-      diftc = '';
-      link = 'https://twitter.com/SeaanONeill';
+      diftc = "";
+      link = "https://twitter.com/SeaanONeill";
       adder = -30;
     }
 
-   c = new Coordinates(125 + this.my + ((windowWidth - width) / 2), this.my + ((windowHeight - height) / 2) + adder, diftc, link);
-   // coords.push(c);
-  //  print(chooser);
- }
+    c = new Coordinates(
+      125 + this.my + (windowWidth - width) / 2,
+      this.my + (windowHeight - height) / 2 + adder,
+      diftc,
+      link
+    );
+    // coords.push(c);
+    //  print(chooser);
+  };
 
   // ----------------------------------------------------------------------------
 
-  this.shuffle = function() {
+//   this.shuffle = function () {
+//     for (var i = 0; i < 10; i++) {
+//       var choose = random(15);
+//       if (choose < 7) {
+//         listpoints[i] = floor(random(350));
+//       }
 
+//       if (choose > 7) {
+//         listpoints2[i] = floor(random(350));
+//       }
+//     }
+//   };
 
+    this.shuffle = function () {
     for (var i = 0; i < 10; i++) {
       var choose = random(15);
       if (choose < 7) {
-        listpoints[i] = floor(random(350));
+        listpoints[i] = floor(random(width * 0.5)) + width * 0.25;
       }
 
       if (choose > 7) {
-        listpoints2[i] = floor(random(350));
+        listpoints2[i] = floor(random(height * 0.5)) + height * 0.25;
       }
     }
-  }
-
+  };
   // ------------------------------------------------------------------------------------
 
-  this.playlist = function() {
-
+  this.playlist = function () {
     duration = song.duration(); // tried this in setup didn't work, why?
-    var jumper = constrain(random(duration), 0, (duration) - 10);
+    var jumper = constrain(random(duration), 0, duration - 10);
     duration3 = song3.duration(); // tried this in setup didn't work, why?
-    var jumper3 = constrain(random(duration3), 0, (duration3) - 39);
+    var jumper3 = constrain(random(duration3), 0, duration3 - 39);
 
     var chooser = floor(random(3));
     if (chooser === 0) {
@@ -305,89 +341,80 @@ endShape();
       env.play();
     }
     if (chooser === 2) {
-     // song3.jump(jumper3, 22);
+      // song3.jump(jumper3, 22);
       song3.play();
       env3.play();
     }
     // song.jump(jumper, 7); // with the jump function, play function isn't needed
+  };
 
-  }
-
-  this.playlist1 = function() {
-
+  this.playlist1 = function () {
     duration1 = song1.duration(); // tried this in setup didn't work, why?
-    var jumper1 = constrain(random(duration1), 0, (duration1) - 25);
+    var jumper1 = constrain(random(duration1), 0, duration1 - 25);
     duration5 = song5.duration(); // tried this in setup didn't work, why?
-    var jumper5 = constrain(random(duration5), 0, (duration5) - 29);
+    var jumper5 = constrain(random(duration5), 0, duration5 - 29);
 
     var chooser2 = floor(random(3));
     if (chooser2 === 0) {
-
       //song1.jump(jumper1, 11); // with the jump function, play function isn't needed // doesnt need the length?
       song2.play();
       env1.play();
     }
     if (chooser2 === 1) {
-
       //song5.jump(jumper5, 22); // with the jump function, play function isn't needed
       song5.play();
       env4.play();
     }
-     if (chooser === 2) {
-     // song3.jump(jumper3, 22);
+    if (chooser === 2) {
+      // song3.jump(jumper3, 22);
       song4.play();
       env2.play();
     }
-  }
+  };
 
-  this.playlist2 = function() {
-
+  this.playlist2 = function () {
     duration2 = song2.duration(); // tried this in setup didn't work, why?
-    var jumper2 = constrain(random(duration2), 0, (duration2) - 9); // removed the floor function to all
+    var jumper2 = constrain(random(duration2), 0, duration2 - 9); // removed the floor function to all
 
-    if (song2.isPlaying()) { // .isPlaying() returns a boolean
+    if (song2.isPlaying()) {
+      // .isPlaying() returns a boolean
       // song.setVolume(0.9, 0.2);
       // song2.stop();
     }
     song2.jump(jumper2, 4); // with the jump function, play function isn't needed
     // song2.play();
     env2.play();
+  };
 
-  }
-  
   function Coordinates(x, y, diftc, link) {
-  
-  this.x = x;
-  this.y = y;
-  this.diftc = diftc;
-  this.link = link;
-  this.txtt = createA(this.link, this.diftc); //
-  this.lifespan2 = 1;
+    this.x = x;
+    this.y = y;
+    this.diftc = diftc;
+    this.link = link;
+    this.txtt = createA(this.link, this.diftc); //
+    this.lifespan2 = 1;
 
-  this.display = function() {
+    this.display = function () {
+      this.txtt.class("fuck"); // cool created a css .fuck // then added safari code in html
+      this.txtt.style("opacity", this.lifespan); // can't figure out how to run fct in mouseover
+      this.txtt.position(this.x, this.y);
+      // this.lifespan += -0.1;
+    };
 
-    this.txtt.class("fuck"); // cool created a css .fuck // then added safari code in html
-    this.txtt.style("opacity", this.lifespan); // can't figure out how to run fct in mouseover
-    this.txtt.position(this.x, this.y);
-    // this.lifespan += -0.1;
-
+    this.update = function () {
+      this.txtt.style("opacity", this.lifespan2); // can't figure out how to run fct in mouseover
+      this.lifespan2 = this.lifespan2 - 0.007; // -0.01 doesnt seem to leave the shadows
+      if (this.lifespan2 <= 0) {
+        this.txtt.remove(); // this got rid of object that was transparent but still 'clickable'
+      }
+    };
   }
 
-  this.update = function() {
-    this.txtt.style("opacity", this.lifespan2); // can't figure out how to run fct in mouseover
-    this.lifespan2 = this.lifespan2 - 0.007; // -0.01 doesnt seem to leave the shadows 
-    if(this.lifespan2 <= 0) {
-      this.txtt.remove(); // this got rid of object that was transparent but still 'clickable'
-    }
-  }
-}
+  // fade out wasnt working, just increased in opacity .. had txtt.createA('#', this.diftc);
+  // needed to add this.txtt.createA..-- in order for the fade (lifespan to work)
 
-// fade out wasnt working, just increased in opacity .. had txtt.createA('#', this.diftc);
-// needed to add this.txtt.createA..-- in order for the fade (lifespan to work)
-
-
-// make text instead of links
-// whats going on with the shadow from the text
-// make actual links for people to click on
-//
-}
+  // make text instead of links
+  // whats going on with the shadow from the text
+  // make actual links for people to click on
+  //
+};
